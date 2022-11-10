1f5d6e1ec6473108852ed4ffd589286abd9477a6
Rodrigo Cardoso <87910366+RodrigoCardos@users.noreply.github.com>
2022-11-09 22:00:49 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,16 @@
+//var, let, const
+
+//var - ES5 - cross-browser
+//let e const -> ES2015
+
+let teste2 = 10;
+
+let teste3; // podemos criar uma varíavel e dar um valor a ela depois, enquanto isso o tipo dessa varíavel será undefined ou null
+console.log(teste2);
+teste3 = 20;
+
+console.log(teste3); // imprimindo depois de dar um valor a varíavel
+const teste4 = "eu sou imutavel" //const
+console.log(teste4);
+
+// usaremos const apenas quando não quisermos mudar a varíavel que estamos criando
