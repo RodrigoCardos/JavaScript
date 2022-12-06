577b649525c47c30b9086f904c79590be5146729
Rodrigo Cardoso <87910366+RodrigoCardos@users.noreply.github.com>
2022-12-06 10:14:48 -0300

    Add files via upload

---
+++ condicional-switch.js
@@ -0,0 +1,47 @@
+let diaSemana = 3
+if (diaSemana === 0){
+    console.log("Hoje é domingo")
+} else if (diaSemana === 1){
+    console.log("Hoje é segunda")
+} else if (diaSemana === 2){
+    console.log("Hoje é terça")
+} else if (diaSemana === 3){
+    console.log("Hoje é quarta")
+} else if (diaSemana === 4){
+    console.log("Hoje é quinta")
+} else if (diaSemana === 5){
+    console.log("Hoje é sexta")
+} else if (diaSemana === 6){
+    console.log("hoje é sábado")
+} else {
+    console.log("hoje é -- ")
+}
+
+
+dia = ""
+switch (diaSemana){
+    case 0:
+        dia = "domingo"
+        break
+    case 1:
+        dia = "segunda"
+        break
+    case 2:
+        dia = "terça"
+        break
+    case 3:
+        dia = "quarta"
+        break
+    case 4:
+        dia = "quinta"
+        break
+    case 5:
+        dia = "sexta"
+        break
+    case 6:
+        dia = "sábado"
+        break
+    default:
+        dia = "hoje é -- "
+}
+console.log(`hoje é ${dia}`)
